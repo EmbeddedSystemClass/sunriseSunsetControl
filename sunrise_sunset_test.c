a80d9bf2901613277e38c31793f80293f5d919ed
Richard Kubíček <rici.kubicek@gmail.com>
2019-12-03 13:21:54 +0100

    repair

--- sunrise_sunset_test.c
+++ sunrise_sunset_test.c
@@ -10,9 +10,14 @@ bool calulateOutputState(uint8_t day, uint8_t month, uint8_t year, uint8_t hours
     int16_t UTCsec;
     struct AbsTime_s time;
     bool _state = false;
+    RTC_TIME tm;
+    RTC_DATE dt;
 
     time.Millisec = 0;
     time.Sec = DateTime_TotalSec(day, month, year, hours, minutes, seconds);
+
+    AbsTime_ToTime(time.Sec, time.Millisec, &tm);
+    AbsTime_ToDate(time.Sec, &dt);
 
     UTCsec = time.Sec;
 
@@ -20,13 +25,13 @@ bool calulateOutputState(uint8_t day, uint8_t month, uint8_t year, uint8_t hours
     UTCsec = time.Sec - UTCsec;
 
     struct SunPhase_s phase;
-    Sun_Phase(Date_DayOfYear(day, month, year), UTCsec / 60, &phase, LONGTITUDE, LONGTITUDE);
+    Sun_Phase(Date_DayOfYear(dt.day, dt.month, dt.year), UTCsec / 60, &phase, LONGTITUDE, LONGTITUDE);
 
-    if ((minutes + hours * 60) >= phase.sunrise)
+    if ((tm.minutes + tm.hours * 60) >= phase.sunrise)
         _state = false;
     else
         _state = true;
-    if ((minutes + hours * 60) >= phase.sunset)
+    if ((tm.minutes + tm.hours * 60) >= phase.sunset)
         _state = true;
 
     return _state;
